3a46557ccd81e1a4fb5c7af7a4b11f7ab2ce6f77
andy <andy@andydote.co.uk>
2015-06-14 13:17:23 +0100

    tests: add scratchpad

---
+++ Ledger.Tests/Scratchpad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Ledger.Tests
+{
+	public class Scratchpad
+	{
+		private readonly ITestOutputHelper _output;
+
+		public Scratchpad(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void When_testing_something()
+		{
+
+		}
+
+	}
+}
