70d85f2e3ba5ced804330586c90bc5d733e2479f
andy <andy@andydote.co.uk>
2015-06-26 08:05:19 +0100

    stores.fs: test 'getLatestEvents'

--- Ledger.Stores.Fs.Tests/EventSaveLoadTests.cs
+++ Ledger.Stores.Fs.Tests/EventSaveLoadTests.cs
@@ -53,6 +53,42 @@ namespace Ledger.Stores.Fs.Tests
 			loaded.Single().ShouldBeOfType<FixNameSpelling>();
 		}
 
+		[Fact]
+		public void Only_the_latest_sequence_is_returned()
+		{
+			var first = Guid.NewGuid();
+			var second = Guid.NewGuid();
+
+			var store = new FileEventStore(_root);
+			store.SaveEvents(first, new[] { new FixNameSpelling { SequenceID = 4 } });
+			store.SaveEvents(first, new[] { new FixNameSpelling { SequenceID = 5 } });
+			store.SaveEvents(second, new[] { new NameChangedByDeedPoll { SequenceID = 6 } });
+
+			store
+				.GetLatestSequenceFor(first)
+				.ShouldBe(5);
+		}
+
+		[Fact]
+		public void Loading_events_since_only_gets_events_after_the_sequence()
+		{
+			var toSave = new DomainEvent[]
+			{
+				new NameChangedByDeedPoll { SequenceID = 3 },
+				new FixNameSpelling { SequenceID = 4 },
+				new FixNameSpelling { SequenceID = 5 },
+				new FixNameSpelling { SequenceID = 6 },
+			};
+
+			var id = Guid.NewGuid();
+			var store = new FileEventStore(_root);
+			store.SaveEvents(id, toSave);
+
+			var loaded = store.LoadEventsSince(id, 4);
+
+			loaded.Select(x => x.SequenceID).ShouldBe(new[] { 5, 6 });
+		}
+
 		public void Dispose()
 		{
 			try
