ed45ae0d881b56a3c583e6f4961be98e79e7137f
andy <andy@andydote.co.uk>
2015-06-27 16:25:59 +0100

    tests: reference store.fs for demo of store acceptance testing

--- Ledger.Tests/AcceptanceTests/AcceptanceBase.cs
+++ Ledger.Tests/AcceptanceTests/AcceptanceBase.cs
@@ -11,5 +11,31 @@ namespace Ledger.Tests.AcceptanceTests
 		{
 			EventStore = new FakeEventStore();
 		}
+
+		//comment out the ctor above and uncomment this to run
+		//against the file system rather than in memory. Useful
+		//to check a new IEventStore implementation works properly.
+
+
+		//private readonly string _root;
+
+		//public AcceptanceBase()
+		//{
+		//	_root = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+		//	Directory.CreateDirectory(_root);
+
+		//	EventStore = new FileEventStore(_root);
+		//}
+
+		//public void Dispose()
+		//{
+		//	try
+		//	{
+		//		Directory.Delete(_root, true);
+		//	}
+		//	catch (Exception)
+		//	{
+		//	}
+		//}
 	}
 }
