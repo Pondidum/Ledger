f41d01c860b6fca1d36fed446d50aa483ed566f0
andy <andy@andydote.co.uk>
2015-07-01 21:44:48 +0100

    store.fs: refactor snapshots save+load tests

--- Ledger.Stores.Fs.Tests/SnapshotSaveLoadTests.cs
+++ Ledger.Stores.Fs.Tests/SnapshotSaveLoadTests.cs
@@ -9,23 +9,24 @@ namespace Ledger.Stores.Fs.Tests
 	public class SnapshotSaveLoadTests : IDisposable
 	{
 		private readonly string _root;
+		private readonly FileEventStore<Guid> _store;
 
 		public SnapshotSaveLoadTests()
 		{
 			_root = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
 
 			Directory.CreateDirectory(_root);
+			_store = new FileEventStore<Guid>(_root);
 		}
 
 		[Fact]
 		public void A_snapshot_should_maintain_type()
 		{
 			var id = Guid.NewGuid();
-			var store = new FileEventStore<Guid>(_root);
 
-			store.SaveSnapshot(id, new CandidateMemento());
+			_store.SaveSnapshot(id, new CandidateMemento());
 
-			var loaded = store.LoadLatestSnapshotFor(id);
+			var loaded = _store.LoadLatestSnapshotFor(id);
 
 			loaded.ShouldBeOfType<CandidateMemento>();
 		}
@@ -34,12 +35,11 @@ namespace Ledger.Stores.Fs.Tests
 		public void Only_the_latest_snapshot_should_be_loaded()
 		{
 			var id = Guid.NewGuid();
-			var store = new FileEventStore<Guid>(_root);
 
-			store.SaveSnapshot(id, new CandidateMemento { SequenceID = 4 });
-			store.SaveSnapshot(id, new CandidateMemento { SequenceID = 5 });
+			_store.SaveSnapshot(id, new CandidateMemento { SequenceID = 4 });
+			_store.SaveSnapshot(id, new CandidateMemento { SequenceID = 5 });
 
-			store
+			_store
 				.LoadLatestSnapshotFor(id)
 				.SequenceID
 				.ShouldBe(5);
@@ -49,12 +49,11 @@ namespace Ledger.Stores.Fs.Tests
 		public void The_most_recent_snapshot_id_should_be_found()
 		{
 			var id = Guid.NewGuid();
-			var store = new FileEventStore<Guid>(_root);
 
-			store.SaveSnapshot(id, new CandidateMemento { SequenceID = 4 });
-			store.SaveSnapshot(id, new CandidateMemento { SequenceID = 5 });
+			_store.SaveSnapshot(id, new CandidateMemento { SequenceID = 4 });
+			_store.SaveSnapshot(id, new CandidateMemento { SequenceID = 5 });
 
-			store
+			_store
 				.GetLatestSnapshotSequenceFor(id)
 				.ShouldBe(5);
 		}
@@ -64,9 +63,8 @@ namespace Ledger.Stores.Fs.Tests
 		public void When_there_is_no_snapshot_file_and_load_is_called()
 		{
 			var id = Guid.NewGuid();
-			var store = new FileEventStore<Guid>(_root);
 
-			var loaded = store.LoadLatestSnapshotFor(id);
+			var loaded = _store.LoadLatestSnapshotFor(id);
 
 			loaded.ShouldBe(null);
 		}
