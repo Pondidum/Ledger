9a2d81ddb715b41dfdff4cb95462ee86ca1a6ab2
andy <andy@andydote.co.uk>
2015-07-06 20:46:36 +0100

    store.postgres: implement int32 keyed aggregate table creation

---
+++ Ledger.Stores.Postgres/CreateIntAggregateTablesCommand.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace Ledger.Stores.Postgres
+{
+	public class CreateIntAggregateTablesCommand
+	{
+		private const string Sql = @"
+create table if not exists events_int32 (
+	id serial primary key,
+	aggregateID int not null,
+	sequence integer not null,
+	event json not null
+);
+
+create table if not exists snapshots_int32 (
+	id serial primary key,
+	aggregateID int not null,
+	sequence integer not null,
+	snapshot json not null
+);
+";
+
+		private readonly NpgsqlConnection _connection;
+
+		public CreateIntAggregateTablesCommand(NpgsqlConnection connection)
+		{
+			_connection = connection;
+		}
+
+		public void Execute()
+		{
+			_connection.Execute(Sql);
+		}
+	}
+}
--- Ledger.Stores.Postgres/ITableName.cs
+++ Ledger.Stores.Postgres/ITableName.cs
@@ -5,17 +5,4 @@
 		string ForEvents<TKey>();
 		string ForSnapshots<TKey>();
 	}
-
-	public class KeyTypeTableName : ITableName
-	{
-		public string ForEvents<TKey>()
-		{
-			return "events_" + typeof(TKey).Name.ToLower();
-		}
-
-		public string ForSnapshots<TKey>()
-		{
-			return "snapshots_" + typeof(TKey).Name.ToLower();
-		}
-	}
 }
