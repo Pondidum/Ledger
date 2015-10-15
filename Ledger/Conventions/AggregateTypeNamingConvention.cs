cc49d5c7c003c2bc7291d7e4dc6653613d51c518
andy <andy@andydote.co.uk>
2015-10-15 20:57:24 +0100

    namingConventions: add aggregateType namingConvention

---
+++ Ledger/Conventions/AggregateTypeNamingConvention.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ledger.Conventions
+{
+	public class AggregateTypeNamingConvention : IStoreNamingConvention
+	{
+		public string ForEvents(Type key, Type aggregate)
+		{
+			return aggregate.Name.ToLower() + "_events";
+		}
+
+		public string ForSnapshots(Type key, Type aggregate)
+		{
+			return aggregate.Name.ToLower() + "_snapshots";
+		}
+	}
+}
