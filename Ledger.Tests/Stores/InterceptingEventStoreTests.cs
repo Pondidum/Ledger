7ed9866f1a947d3f22318890e9c4f5576ea1bca5
andy <andy@andydote.co.uk>
2015-11-16 20:08:03 +0000

    stores: add interceptingEventStore to help with capturing event streams etc

---
+++ Ledger.Tests/Stores/InterceptingEventStoreTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Ledger.Acceptance.TestObjects;
+using Ledger.Infrastructure;
+using Ledger.Stores;
+using Shouldly;
+using Xunit;
+
+namespace Ledger.Tests.Stores
+{
+	public class InterceptingEventStoreTests
+	{
+
+		[Fact]
+		public void When_an_event_occours()
+		{
+			var events = new List<IDomainEvent>();
+			var store = new InMemoryEventStore<Guid>();
+			var wrapped = new EventLoggingStore(store, e => events.Add(e));
+
+			var aggregate = new TestAggregate();
+			aggregate.GenerateID();
+
+			var event1 = new TestEvent();
+			aggregate.AddEvent(event1);
+
+			var ags = new AggregateStore<Guid>(wrapped);
+			ags.Save(aggregate);
+
+			events.ShouldBe(new[] { event1 });
+			store.AllEvents.ShouldBe(new[] { event1 });
+
+		}
+
+		private class EventLoggingStore : InterceptingEventStore<Guid>
+		{
+			private readonly Action<IDomainEvent> _onEvent;
+
+			public EventLoggingStore(IEventStore<Guid> other, Action<IDomainEvent> onEvent)
+				: base(other)
+			{
+				_onEvent = onEvent;
+			}
+
+			public override void SaveEvents(IStoreConventions storeConventions, Guid aggregateID, IEnumerable<IDomainEvent> changes)
+			{
+				//this is a pretty bad impl, as you can block event saving, but its easy to test!
+				//also using the .Apply() method avoids iterating the changes collection more than once.
+
+				base.SaveEvents(storeConventions, aggregateID, changes.Apply(change => _onEvent(change)));
+			}
+		}
+	}
+}
---
+++ Ledger/Stores/InterceptingEventStore.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Ledger.Stores
+{
+	public class InterceptingEventStore<T> : IEventStore<T>
+	{
+		private readonly IEventStore<T> _other;
+
+		public InterceptingEventStore(IEventStore<T> other)
+		{
+			_other = other;
+		}
+
+		public virtual void Dispose()
+		{
+			_other.Dispose();
+		}
+
+		public virtual int? GetLatestSequenceFor(IStoreConventions storeConventions, T aggregateID)
+		{
+			return _other.GetLatestSequenceFor(storeConventions, aggregateID);
+		}
+
+		public virtual int? GetLatestSnapshotSequenceFor(IStoreConventions storeConventions, T aggregateID)
+		{
+			return _other.GetLatestSnapshotSequenceFor(storeConventions, aggregateID);
+		}
+
+		public virtual void SaveEvents(IStoreConventions storeConventions, T aggregateID, IEnumerable<IDomainEvent> changes)
+		{
+			_other.SaveEvents(storeConventions, aggregateID, changes);
+		}
+
+		public virtual IEnumerable<IDomainEvent> LoadEvents(IStoreConventions storeConventions, T aggregateID)
+		{
+			return _other.LoadEvents(storeConventions, aggregateID);
+		}
+
+		public virtual IEnumerable<IDomainEvent> LoadEventsSince(IStoreConventions storeConventions, T aggregateID, int sequenceID)
+		{
+			return _other.LoadEventsSince(storeConventions, aggregateID, sequenceID);
+		}
+
+		public virtual ISequenced LoadLatestSnapshotFor(IStoreConventions storeConventions, T aggregateID)
+		{
+			return _other.LoadLatestSnapshotFor(storeConventions, aggregateID);
+		}
+
+		public virtual void SaveSnapshot(IStoreConventions storeConventions, T aggregateID, ISequenced snapshot)
+		{
+			_other.SaveSnapshot(storeConventions, aggregateID, snapshot);
+		}
+
+		public virtual IEventStore<T> BeginTransaction()
+		{
+			_other.BeginTransaction();
+			return this;
+		}
+	}
+}
