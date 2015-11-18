f27c665255b78fc9a6112857531236d7fbefb71e
andy <andy@andydote.co.uk>
2015-11-18 22:05:49 +0000

    tests: remove structuremap, implement new acceptance tests

---
+++ Ledger.Tests/Acceptance.cs
@@ -0,0 +1,14 @@
+﻿using Ledger.Acceptance.AcceptanceTests;
+using Ledger.Stores;
+using Xunit;
+
+namespace Ledger.Tests
+{
+	public class Acceptance : AcceptanceTests
+	{
+		public Acceptance() 
+			: base(new InMemoryEventStore())
+		{
+		}
+	}
+}
