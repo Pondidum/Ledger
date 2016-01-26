d895b01e685460e0998d05ded73a6d59be5b3172
andy <andy@andydote.co.uk>
2016-01-26 19:57:40 +0000

    migrator: add object to assist migration from one store to another

---
+++ Ledger.Tests/MigratorTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Ledger.Acceptance;
+using Ledger.Acceptance.TestObjects;
+using Ledger.Infrastructure;
+using Ledger.Stores;
+using Shouldly;
+using Xunit;
+
+namespace Ledger.Tests
+{
+	public class MigratorTests
+	{
+		private static readonly EventStoreContext TestStream = new EventStoreContext("TestStream", Default.SerializerSettings);
+		private readonly IncrementingStamper _stamper;
+
+		public MigratorTests()
+		{
+			_stamper = new IncrementingStamper();
+		}
+
+		private void WriteEvents(InMemoryEventStore store, params IDomainEvent<Guid>[] events)
+		{
+			var aggregateID = Guid.NewGuid();
+
+			using (var writer = store.CreateWriter<Guid>(TestStream))
+			{
+				writer.SaveEvents(events
+					.Apply(e => e.Stamp = _stamper.GetNext())
+					.Apply(e => e.AggregateID = aggregateID));
+			}
+		}
+
+		private void WriteSnapshots(InMemoryEventStore store, params ISnapshot<Guid>[] snapshots)
+		{
+			var aggregateID = Guid.NewGuid();
+
+			using (var writer = store.CreateWriter<Guid>(TestStream))
+			{
+				snapshots
+					.Apply(s => s.Stamp = _stamper.GetNext())
+					.Apply(s => s.AggregateID = aggregateID)
+					.ForEach(s => writer.SaveSnapshot(s));
+			}
+		}
+
+		[Fact]
+		public void When_migrating_events_only_to_a_new_stream()
+		{
+			var source = new InMemoryEventStore();
+			var dest = new InMemoryEventStore();
+
+			WriteEvents(source, new TestEvent(), new TestEvent(), new TestEvent());
+
+			var migrator = new Migrator(source, dest);
+			migrator.ToEmptyStream<Guid>(TestStream.StreamName);
+
+			dest.AllEvents.Count().ShouldBe(3);
+		}
+
+		[Fact]
+		public void When_migrating_snapshots_only_to_a_new_stream()
+		{
+			var source = new InMemoryEventStore();
+			var dest = new InMemoryEventStore();
+
+			WriteSnapshots(source, new TestSnapshot());
+
+
+			var migrator = new Migrator(source, dest);
+			migrator.ToEmptyStream<Guid>(TestStream.StreamName);
+
+			dest.AllSnapshots.Count().ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_migrating_snapshots_only_latest_is_kept()
+		{
+			var source = new InMemoryEventStore();
+			var dest = new InMemoryEventStore();
+
+			WriteSnapshots(source, new TestSnapshot(), new TestSnapshot(), new TestSnapshot());
+
+
+			var migrator = new Migrator(source, dest);
+			migrator.ToEmptyStream<Guid>(TestStream.StreamName);
+
+			dest.AllSnapshots.Count().ShouldBe(1);
+		}
+	}
+}
---
+++ Ledger/Migrator.cs
@@ -0,0 +1,45 @@
+﻿using Ledger.Infrastructure;
+using Newtonsoft.Json;
+
+namespace Ledger
+{
+	public class Migrator
+	{
+		private readonly IEventStore _source;
+		private readonly IEventStore _destination;
+		private readonly JsonSerializerSettings _settings;
+
+		public Migrator(IEventStore source, IEventStore destination)
+			: this(source, destination, null)
+		{
+		}
+
+		public Migrator(IEventStore source, IEventStore destination, JsonSerializerSettings serializerSettings)
+		{
+			_source = source;
+			_destination = destination;
+			_settings = serializerSettings ?? Default.SerializerSettings;
+		}
+
+		public void ToEmptyStream<TKey>(string streamName)
+		{
+			var context = new EventStoreContext(streamName, _settings);
+
+			using (var reader = _source.CreateReader<TKey>(context))
+			using (var writer = _destination.CreateWriter<TKey>(context))
+			{
+				var keys = reader.LoadAllKeys();
+
+				foreach (var key in keys)
+				{
+					writer.SaveEvents(reader.LoadEvents(key));
+
+					var snapshot = reader.LoadLatestSnapshotFor(key);
+
+					if (snapshot != null)
+						writer.SaveSnapshot(snapshot);
+				}
+			}
+		}
+	}
+}
