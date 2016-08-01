eebbde9855738bbdab04269f41306368f0c762b2
andy <andy@andydote.co.uk>
2016-08-01 15:17:42 +0100

    tests: add requiresRabbitFact attribute

---
+++ Ledger.Tests/TestInfrastructure/RequiresRabbitFactAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RabbitMQ.Client;
+using Xunit;
+
+namespace Ledger.Tests.TestInfrastructure
+{
+	public class RequiresRabbitFactAttribute : FactAttribute
+	{
+		public RequiresRabbitFactAttribute()
+		{
+			if (IsRabbitAvailable.Value == false)
+			{
+				Skip = "Postgres is not available";
+			}
+		}
+
+		private static readonly Lazy<bool> IsRabbitAvailable;
+
+		static RequiresRabbitFactAttribute()
+		{
+			IsRabbitAvailable = new Lazy<bool>(() =>
+			{
+				try
+				{
+					var factory = new ConnectionFactory
+					{
+						HostName = "10.0.75.2",
+						RequestedConnectionTimeout = 1000
+					};
+					
+					using (var connection = factory.CreateConnection())
+					{
+						return connection.IsOpen;
+					}
+				}
+				catch (Exception)
+				{
+					return false;
+				}
+			});
+		}
+
+	}
+}
