fa24e57d4a24fd0a73d916a341fb2b8a36493d3d
andy <andy@andydote.co.uk>
2016-08-07 13:00:19 +0100

    tests: add tests for .asDynamic() invocation

---
+++ Ledger.Tests/DynamicInvocationTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ledger.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace Ledger.Tests
+{
+	public class DynamicInvocationTests
+	{
+		private readonly TestTarget _target = new TestTarget();
+
+		[Fact]
+		public void When_invoking_a_parent_class()
+		{
+			_target.AsDynamic().Handle(new Parent());
+			_target.Invoked.ShouldBe("parent");
+		}
+
+		[Fact]
+		public void When_invoking_a_child_class()
+		{
+			_target.AsDynamic().Handle(new Child());
+			_target.Invoked.ShouldBe("child");
+		}
+
+		[Theory]
+		[MemberData("Targets")]
+		public void When_invoking_as_cast_to_object(object arg, string expected)
+		{
+			_target.AsDynamic().Handle(arg);
+			_target.Invoked.ShouldBe(expected);
+		}
+
+		public static IEnumerable<object[]> Targets
+		{
+			get
+			{
+				yield return new object[] { new Parent(), "parent" };
+				yield return new object[] { new Child(), "child" };
+				yield return new object[] { new GrandChild(), "grandchild" };
+				yield return new object[] { new OtherChild(), "parent" };
+			}
+		}
+
+		public class TestTarget
+		{
+			public string Invoked { get; private set; }
+
+			private void Handle(Parent e)
+			{
+				Invoked = "parent";
+			}
+
+			private void Handle(Child e)
+			{
+				Invoked = "child";
+			}
+
+			private void Handle(GrandChild e)
+			{
+				Invoked = "grandchild";
+			}
+		}
+
+		public class Parent { }
+		public class Child : Parent { }
+		public class GrandChild : Child { }
+		public class OtherChild : Parent { }
+	}
+}
